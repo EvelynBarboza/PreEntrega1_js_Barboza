694d8330f2d8fa60e4dfae6a206eb95447f4991c
EvelynBarboza <natyevy12@gmail.com>
2023-07-25 14:13:09 -0300

    ayuda

--- js/main.js
+++ js/main.js
@@ -4,32 +4,48 @@ alert("Bienvenidos a ADIVINA EL NUMERO")
 
 
 //LLAMO A LA FUNCION
+//let tope = contador
+//for (let contador=0; contador < ; contador +=1)
+//let mostrar = miarray [contador]
+//console.log ("Intentos", contador.slice (0,2))
+/*let datosJugador = {
+    nombre: prompt("Ingresa tu nombre"),
+    apellido: prompt("Ingresa tu apellido")
+}*/
+
+//console.log (datosJugador.nombre)
+
+//LLAMO A LA FUNCION
 adivinarNumero()
 
 //FUNCION
 function adivinarNumero() {
+    let datosJugador = {
+        nombre: prompt("Ingresa tu nombre"),
+        apellido: prompt("Ingresa tu apellido")
+    } 
 
     let numeroX = Math.ceil(Math.random()*10);
     console.log(numeroX)
     let ganaste = 0
-    for (let contador=0; contador < 3; contador +=1) {
+    for (let i=0; i < 3; i +=1) {
         let intento = prompt("Ingresa un nùmero");
         //let ok = validarNumero(numeroX)
         if (numeroX == intento) {
-            alert("Felicidades, Ganaste Rey");
+            alert("Felicidades, Ganaste");
             ganaste = 1
             break;
         }else{ 
             if (numeroX > intento) {
-                console.log("Debes ingresar un nùmero mayor");
+                console.log(datosJugador.nombre + " debes ingresar un nùmero mayor");
             }else{  
-                console.log("Debes ingresar un nùmero menor")
+                console.log(datosJugador.nombre + " debes ingresar un nùmero menor");
             }    
         }
-    } 
-    if (ganaste=0) {
-        alert("GAME OVER")
     }
+    if (ganaste = 0) {
+        alert("GAME OVER")
+    } 
     return;
 }
 
